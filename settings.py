dbb92363dd47ba02cb28fffc2444fd06226932c5
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-04-02 17:04:38 +0200

    Small changes in KeyLogger

--- settings.py
+++ settings.py
@@ -2,3 +2,6 @@
 email_address = " "  # Enter disposable email here
 password = " "  # Enter email password here
 to_address = " "  # Enter the email address you want to send your information to
+
+time_iteration = 15
+number_of_iterations_end = 3
