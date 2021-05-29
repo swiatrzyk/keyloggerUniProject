943fbbe5303f5f17614b48d6bde384530b439ea6
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-05-29 15:21:34 +0200

    UT keylogger init

---
+++ UT/test_KeyLogger.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestKeyLogger(TestCase):
+    def test_run(self):
+        self.fail()
+
+    def test_on_press(self):
+        self.fail()
+
+    def test_write_file(self):
+        self.fail()
+
+    def test_on_release(self):
+        self.fail()
+
+    def test_after_stop(self):
+        self.fail()
